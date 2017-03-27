b3b0918b37d8c1689dd94b676cbc39f274191c95
hexnor <cse.ykr@gmail.com>
2017-03-27 01:28:53 -0500

    heroku

--- Hostel_Management_system/settings.py
+++ Hostel_Management_system/settings.py
@@ -97,6 +97,6 @@ REST_FRAMEWORK = {
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'

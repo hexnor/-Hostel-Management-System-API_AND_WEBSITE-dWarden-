ef9882a43f401fe8c8860d4a4414c6f974ff195c
hexnor <cse.ykr@gmail.com>
2017-03-27 02:00:28 -0500

    Done

--- Hostel_Management_system/wsgi.py
+++ Hostel_Management_system/wsgi.py
@@ -1,5 +1,6 @@
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Hostel_Management_system.settings'
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
